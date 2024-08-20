52693c1b2db2cba5a4991a46337d2ffb224dd1be
Vladimir NG <potd3@ukr.net>
2024-08-20 21:00:17 +0300

    add deleteIdVideo + indexHandler

---
+++ src/handlerEndpoints/deleteIdVideoController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import {db} from '../db/db';
+import {SETTING, HTTP_STATUSES} from '../setting';
+
+export const deleteIdVideoController = (req: Request, res: Response) =>{
+    
+    const index = db.videos.findIndex(n => n.id === +req.params.id);
+  
+    if(index == -1){
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
+    }
+    
+    db.videos = db.videos.filter(n => n.id !== +req.params.id);
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+    
+}
---
+++ src/handlerEndpoints/indexHandlers.ts
@@ -0,0 +1,9 @@
+import { getVideoController } from './getVideoController';
+import { getIdVideoController } from './getIdVideoController';
+import { deleteIdVideoController } from './deleteIdVideoController';
+import { putVideoController } from './putVideoController';
+import { postVideoController } from './postVideoController';
+import { deleteAllController } from './deleteAllController';
+
+
+export{getVideoController, getIdVideoController, deleteIdVideoController, putVideoController, postVideoController, deleteAllController}
