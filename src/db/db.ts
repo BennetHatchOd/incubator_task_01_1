9091e24a0fe5d7cc4924f6dd037241800e032c45
Vladimir NG <potd3@ukr.net>
2024-08-21 19:49:48 +0300

    fix db

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,10 +3,5 @@ export type DBType = {
 };
 
 export const db: DBType = {
-    videos: [
-        {id: 1, name: "tfg"},
-        {id: 2, name: "tf45g"},
-        {id: 3, name: "t456fg"},
-        {id: 4, name: "27tfg"}
-    ]
+    videos: []
 }
